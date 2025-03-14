4b637d739300356dc368769a7e660f7bd6014672
AbrahamOlsen <cabrahamolsenchaves8@gmail.com>
2025-03-13 20:20:25 -0600

    Arreglo visual de Visualizar y Visualizar Lotes
    Se arregla el error de visualizacion en Visualizar y se agrega el componente Visualizar Lotes para poder trabajar simultaneo

---
+++ sistema-registro/src/pages/Visualizar/Registros/PagesVisualizarLotes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import VisualizarLotes from '../../../Components/Visualizacion/Registros/VisualizarLotes.jsx'
+
+
+const PagesVisualizarLotes = () => {
+  return (
+    <>
+        <VisualizarLotes/>
+    </>
+  )
+}
+
+export default PagesVisualizarLotes
